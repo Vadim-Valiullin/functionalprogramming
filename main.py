5480c0955dd3629028910266cb523e437f191e5b
Vadim-Valiullin <113513063+Vadim-Valiullin@users.noreply.github.com>
2022-10-09 19:13:37 +0500

    Изменил формат ввода

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def uniquer(data: list) -> list:
     return list(set(data))
 
 
-def sorter(data: list, value: str) -> list:
-    if value == 1:
+def sorter(data: list, val: str) -> list:
+    if val == 'asc':
         return sorted(data, reverse=False)
-    if value == 2:
+    if val == 'desc':
         return sorted(data, reverse=True)
 
 
@@ -21,18 +21,16 @@ def limiter(data: list, val: str) -> list:
     return [data[i] for i in range(int(val))]
 
 
-def func_data(data: list, com: int, val: str) -> list:
-    if com == 1:
+def func_data(data: list, com: str, val: str) -> list:
+    if com == 'filter':
         return filterer(data, val)
-    elif com == 2 and val.isdigit():
+    elif com == 'map' and val.isdigit():
         return maper(data, val)
-    elif com == 3 and val == '-':
+    elif com == 'unique' and val == '-':
         return uniquer(data)
-    elif com == 4 and val == 1:
+    elif com == 'sort' and val in ['asc', 'desc']:
         return sorter(data, val)
-    elif com == 4 and val == 2:
-        return sorter(data, val)
-    elif com == 5 and val.isdigit():
+    elif com == 'limit' and val.isdigit():
         return limiter(data, val)
     else:
         return []
@@ -42,34 +40,19 @@ def main():
     while True:
         with open('apache_logs.txt') as file:
             data = file.readlines()
-        print('Выберите подходящую команду: \n1. filter - является “поиском” по файлу\n2. map - этой командой мы изменяем формат исходных данных\n3. unique - оставляет только уникальные значения\n4. sort - сортирует данные в '
-              'алфавитном порядке или в обратном алфавитном порядке\n5. limit - выводит '
+        print('Выберите подходящую команду и необходимое значение: \nfilter - является “поиском” по файлу\nmap - этой '
+              'командой мы '
+              'изменяем формат исходных данных\nunique - оставляет только уникальные значения\nsort - сортирует данные в '
+              'алфавитном порядке или в обратном алфавитном порядке\nlimit - выводит '
               'только необходимое количество строк из запроса')
         user_input = input("Введите команду: ")
         commands = user_input.split(' | ')
-        for com in commands:
-            if int(com) == 1:
-                val = input('Ведите строку, по которой нужно будет фильтровать данные: ')
-                data = func_data(data, com, val)
-            elif com == 2:
-                val = input('Выберите нужную колонку данных: ')
-                data = func_data(data, com, val)
-            elif com == 3:
-                val = '-'
-                data = func_data(data, com, val)
-            elif com == 4:
-                val = input('Введите 1, если хотите вывести запрос в алфавитном порядке\nВведите 2, если хотите вывести '
-                            'запрос в обратном алфавитном порядке\nВвод: ')
-                if val == 1 or val == 2:
-                    data = func_data(data, com, val)
-                else:
-                    print('Нужно было ввести 1 или 2\nПовторите запрос сначала.')
-            elif com == 5:
-                val = input('Введите необходимое количество строк запроса: ')
-                data = func_data(data, com, val)
-            else:
-                print('Не верно введенная команда\n')
+        for command in commands:
+            com = command.split()
+            if len(com) != 2:
+                print('Некорректная команда\n')
                 exit()
+            data = func_data(data, com[0], com[1])
             if not data:
                 print('Не верно введенная команда\n')
                 exit()
